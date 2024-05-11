73c49965bc744fbb6ba9f0d3d406e941221b8535
Brandon Davis <wowzerss90@yahoo.com>
2024-05-11 19:42:18 -0400

    Created camera for player, Mapping context and input actions.

--- Source/MultiplayerMadness/Private/Character/MMCharacter.cpp
+++ Source/MultiplayerMadness/Private/Character/MMCharacter.cpp
@@ -3,11 +3,28 @@
 
 #include "Character/MMCharacter.h"
 
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+#include "MovieSceneFwd.h"
+
 // Sets default values
 AMMCharacter::AMMCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	Camera->bUsePawnControlRotation = false;
+
 
 }
 
@@ -15,8 +32,20 @@ AMMCharacter::AMMCharacter()
 void AMMCharacter::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if(APlayerController* PC = Cast<APlayerController>(Controller))
+	{
+		if(UEnhancedInputLocalPlayerSubsystem* InputSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer()))
+		{
+			if(MMMapingContext)
+			{
+				InputSystem->AddMappingContext(MMMapingContext, 0);
+			}
+		}
+	}
 	
 }
+
 
 // Called every frame
 void AMMCharacter::Tick(float DeltaTime)
@@ -25,10 +54,35 @@ void AMMCharacter::Tick(float DeltaTime)
 
 }
 
+
 // Called to bind functionality to input
 void AMMCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+	UEnhancedInputComponent* EnhancedInput = CastChecked<UEnhancedInputComponent>(PlayerInputComponent);
+
+	EnhancedInput->BindAction(MoveAction, ETriggerEvent::Triggered, this, &ThisClass::MovePlayer);
+
+	EnhancedInput->BindAction(JumpAction, ETriggerEvent::Triggered, this, &ThisClass::Jump);
+
 }
 
+void AMMCharacter::MovePlayer(const FInputActionValue& Value)
+{
+	const FVector2D Axis = Value.Get<FVector2d>();
+
+	const FRotator MovementRotation(0, Controller->GetControlRotation().Yaw, 0);
+
+	if (Axis.Y != 0.f)
+	{
+		const FVector Direction = MovementRotation.RotateVector(FVector::ForwardVector);
+		AddMovementInput(Direction, Axis.Y);
+	}
+
+	if (Axis.X != 0.f)
+	{
+		const FVector Direction = MovementRotation.RotateVector(FVector::RightVector);
+		AddMovementInput(Direction, Axis.X);
+	}
+}
--- Source/MultiplayerMadness/Public/Character/MMCharacter.h
+++ Source/MultiplayerMadness/Public/Character/MMCharacter.h
@@ -15,15 +15,37 @@ public:
 	// Sets default values for this character's properties
 	AMMCharacter();
 
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	TObjectPtr<class UInputMappingContext> MMMapingContext;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input)
+	TObjectPtr<class UInputAction> JumpAction;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = Input)
+	TObjectPtr<class UInputAction> MoveAction;
+
+
+private:
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	TObjectPtr<class UCameraComponent> Camera;
+
+	UPROPERTY(VisibleAnywhere, Category = Camera)
+	TObjectPtr<class USpringArmComponent> CameraBoom;
+
+	void MovePlayer(const struct FInputActionValue& Value);
+
+public:	
+
 
 };
